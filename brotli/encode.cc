598f23ed532ffa5cad0051f7d993be7ee3547b74
Khaled Hosny <khaledhosny@eglug.org>
2014-11-15 17:54:32 +0200

    Screw SWIG and Cython
    I just wasted my time fighting there abstractions. Now using the Python
    API directly.

---
+++ brotli/encode.cc
@@ -0,0 +1,59 @@
+#define PY_SSIZE_T_CLEAN 1
+#include <Python.h>
+#include "brotli/enc/encode.h"
+
+static PyObject *BrotliError;
+
+static PyObject * encode_compress(PyObject * self, PyObject * args)
+{
+  PyObject *ReturnVal = NULL;
+  uint8_t *input, *output;
+  size_t length;
+  int mode = -1;
+  PyObject* transform = NULL;
+
+  if (!PyArg_ParseTuple(args, "s#|iO!:compress", &input, &length, &mode, &PyBool_Type, &transform))
+    return NULL;
+
+  size_t output_length = length * 2;
+  output = new uint8_t[output_length];
+
+  brotli::BrotliParams params;
+  if (mode != -1)
+    params.mode = (brotli::BrotliParams::Mode) mode;
+  if (transform)
+    params.enable_transforms = PyObject_IsTrue(transform);
+
+  if (brotli::BrotliCompressBuffer(params, length, input, &output_length, output))
+    {
+      ReturnVal = PyString_FromStringAndSize((char*)output, output_length);
+    }
+  else
+    {
+      PyErr_SetString(BrotliError, "BrotliCompressBuffer failed");
+      goto error;
+    }
+
+error:
+  delete[] output;
+
+  return ReturnVal;
+}
+
+static PyMethodDef encode_methods[] = {
+  {"compress", encode_compress, METH_VARARGS, ""},
+  {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC
+initencode(void)
+{
+  PyObject *m = Py_InitModule("encode", encode_methods);
+
+  BrotliError = PyErr_NewException((char*) "brotli.error", NULL, NULL);
+
+  if (BrotliError != NULL) {
+    Py_INCREF(BrotliError);
+    PyModule_AddObject(m, "error", BrotliError);
+  }
+}
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 
 encode = Extension("brotli.encode",
                     sources=[
-                        "brotli/encode.pyx",
+                        "brotli/encode.cc",
                         "brotli/brotli/enc/backward_references.cc",
                         "brotli/brotli/enc/block_splitter.cc",
                         "brotli/brotli/enc/brotli_bit_stream.cc",
@@ -17,7 +17,7 @@ encode = Extension("brotli.encode",
 
 decode = Extension("brotli.decode",
                     sources=[
-                        "brotli/decode.pyx",
+                        "brotli/decode.c",
                         "brotli/brotli/dec/bit_reader.c",
                         "brotli/brotli/dec/decode.c",
                         "brotli/brotli/dec/huffman.c",
@@ -32,5 +32,5 @@ setup(
     description="Python binding of the Brotli compression library",
     author="Khaled Hosny",
     packages=["brotli"],
-    ext_modules=cythonize([encode, decode]),
+    ext_modules=[encode, decode],
 )
